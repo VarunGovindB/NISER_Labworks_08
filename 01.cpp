c8327b391e91b4a728dbb5b699e03aa6bbcb9507
VarunGovindB <31276825+VarunGovindB@users.noreply.github.com>
2017-11-17 22:09:08 +0530

    Create 01.cpp

---
+++ 01.cpp
@@ -0,0 +1,173 @@
+#include <iostream>
+#include<stdlib.h>
+using namespace std;
+
+class list
+{
+public:
+	struct node
+	{
+		int info;
+		node* next;
+	} *first,*last,*temp;
+
+list()
+{
+	first=NULL;
+	last=NULL;
+	temp=NULL;
+}
+void add()
+{
+    temp=new node;
+    int n;
+    cout<<"\nEnter an Element:";
+    cin>>n;
+    temp->info=n;
+    temp->next=NULL;
+    if(first==NULL)
+    {
+        first=temp;
+        last=first;
+    }
+ 
+    else
+    {
+        last->next=temp;
+        last=temp;
+    }
+}
+
+void insert()
+{
+    node *prev,*cur;
+    prev=NULL;
+    cur=first;
+    int count=1,pos,ch,n;
+    temp=new node;
+    cout<<"\nEnter an Element:";
+    cin>>n;
+    temp->info=n;
+    temp->next=NULL;
+    cout<<"\nINSERT AS\n1:FIRSTNODE\n2:LASTNODE\n3:IN BETWEEN FIRST&LAST NODES";
+    cout<<"\nEnter Your Choice:";
+    cin>>ch;
+    switch(ch)
+    {
+    case 1:
+        temp->next=first;
+        first=temp;
+        break;
+    case 2:
+        last->next=temp;
+        last=temp;
+        break;
+    case 3:
+        cout<<"\nEnter the Position to Insert:";
+        cin>>pos;
+        while(count!=pos)
+        {
+            prev=cur;
+            cur=cur->next;
+            count++;
+        }
+        if(count==pos)
+        {
+            prev->next=temp;
+            temp->next=cur;
+        }
+        else
+            cout<<"\nNot Able to Insert";
+        break;
+ 
+    }
+}
+
+void del()
+{
+    node *prev=NULL,*cur=first;
+    int count=1,pos,ch;
+    cout<<"\nDELETE\n1:FIRSTNODE\n2:LASTNODE\n3:IN BETWEEN FIRST&LAST NODES";
+    cout<<"\nEnter Your Choice:";
+    cin>>ch;
+    switch(ch)
+    {
+    case 1:
+        if(first!=NULL)
+        {
+            cout<<"\nDeleted Element is "<<first->info;
+            first=first->next;
+        }
+        else
+            cout<<"\nNot Able to Delete";
+        break;
+    case 2:
+        while(cur!=last)
+        {
+            prev=cur;
+            cur=cur->next;
+        }
+        if(cur==last)
+        {
+            cout<<"\nDeleted Element is: "<<cur->info;
+            prev->next=NULL;
+            last=prev;
+        }
+        else
+            cout<<"\nNot Able to Delete";
+        break;
+    case 3:
+        cout<<"\nEnter the Position of Deletion:";
+        cin>>pos;
+        while(count!=pos)
+        {
+            prev=cur;
+            cur=cur->next;
+            count++;
+        }
+        if(count==pos)
+        {
+            cout<<"\nDeleted Element is: "<<cur->info;
+            prev->next=cur->next;
+        }
+        else
+            cout<<"\nNot Able to Delete";
+        break;
+    }
+}
+
+void display()
+{
+    node *temp=first;
+    if(temp==NULL)
+    {
+        cout<<"\nList is Empty";
+    }
+    while(temp!=NULL)
+    {
+        cout<<temp->info;
+        cout<<"-->";
+        temp=temp->next;
+    }
+    cout<<"NULL";
+}
+};
+
+int main() 
+{
+        int ch;
+        cout<<"\n1:ADD\n2:INSERT\n3:DELETE\n4:DISPLAY\n5:EXIT\n";
+        cout<<"\nEnter Your Choice:";
+        cin>>ch;
+list a;
+if(ch==1)
+a.add();
+else if(ch==2)
+a.insert();
+else if(ch==3)
+a.del();
+else if(ch==4)
+a.display();
+
+return 0;
+}
